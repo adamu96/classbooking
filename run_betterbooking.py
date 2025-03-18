205efdd5f738c6c4fdf7b1dd363e2192afc9c6cc
Adam <adamu_1996@hotmail.com>
2025-03-18 22:14:55 +0000

    refactor: uncomment email notification for successful tennis booking

--- run_betterbooking.py
+++ run_betterbooking.py
@@ -73,8 +73,8 @@ while not booking_successful:
                             #         start= f'{date}T{str(slot["hour"])}'+':00:00',
                             #         end=f'{date}T{str(slot["hour"]+1)}'+':00:00',
                             #         attendees='margretbarclay10@gmail.com'))
-                            # send_gmail(subject=f"Tennis booked: {date} {str(slot['hour'])}:00",
-                            #            message="Get yourselves out there and have fun.")
+                            send_gmail(subject=f"Tennis booked: {date} {str(slot['hour'])}:00",
+                                       message="Get yourselves out there and have fun.")
                             exit()
                 if booking_successful:
                     break
